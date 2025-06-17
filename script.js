6a573032b4139ca8e5ea8d4fc0187e5fb5a4755f
Hania Shafiq <140416218+Hania-Shafiq@users.noreply.github.com>
2025-06-17 13:33:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
+
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
 
 function opentab(tabname) {
-    // Remove "active-link" class from all tab links
-    for (var tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
+  tabLinks.forEach(link => link.classList.remove("active-link"));
+  tabContents.forEach(content => content.classList.remove("active-tab"));
 
-    // Remove "active-tab" class from all tab contents
-    for (var tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-
-    // Add "active-link" to the clicked tab link
-    event.currentTarget.classList.add("active-link");
-
-    // Add "active-tab" to the corresponding tab content
-    document.getElementById(tabname).classList.add("active-tab");
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
